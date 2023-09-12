7b1dd42aab8a05a55b264e20275caa2af31f44eb
Victoria <victoria.yamashita@estudante.ufjf.br>
2023-09-12 10:05:05 -0300

    third index section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { ArticleIndexPrimary, ArticleIndexSecundary, ArticleIndexTerciary } from "@/components/Article";
+import { ArticleIndexPrimary, ArticleIndexSecundary, ArticleIndexTerciary, ArticleShop } from "@/components/Article";
 import { Navbar } from "@/components/Navbar";
 import { Stack } from "@chakra-ui/react";
 
@@ -11,6 +11,9 @@ export default function Home() {
       <ArticleIndexPrimary></ArticleIndexPrimary>
       <ArticleIndexSecundary></ArticleIndexSecundary>
       <ArticleIndexTerciary></ArticleIndexTerciary>
+      <ArticleShop title={"Nossas Bolsas"} color={"purple"} src1={"/bolsa1.png"} alt1={""} width1={120} height1={125} titleItem1={"Bolsa Redonda"} price1={"R$109,90"} src2={"/bolsa1.png"} alt2={""} width2={120} height2={125} titleItem2={"Bolsa Sun"} price2={"R$129,90"} src3={"/bolsa1.png"} alt3={""} width3={120} height3={125} titleItem3={"Bolsa Pink"} price3={"R$99,90"} colorCard={"lilac"}></ArticleShop>
+      <ArticleShop title={"Nossas Bolsas"} color={"lilac"} src1={"/bolsa1.png"} alt1={""} width1={120} height1={125} titleItem1={"Bolsa Redonda"} price1={"R$109,90"} src2={"/bolsa1.png"} alt2={""} width2={120} height2={125} titleItem2={"Bolsa Sun"} price2={"R$129,90"} src3={"/bolsa1.png"} alt3={""} width3={120} height3={125} titleItem3={"Bolsa Pink"} price3={"R$99,90"} colorCard={"purple"}></ArticleShop>
+      <ArticleShop title={"Nossas Bolsas"} color={"purple"} src1={"/bolsa1.png"} alt1={""} width1={120} height1={125} titleItem1={"Bolsa Redonda"} price1={"R$109,90"} src2={"/bolsa1.png"} alt2={""} width2={120} height2={125} titleItem2={"Bolsa Sun"} price2={"R$129,90"} src3={"/bolsa1.png"} alt3={""} width3={120} height3={125} titleItem3={"Bolsa Pink"} price3={"R$99,90"} colorCard={"lilac"}></ArticleShop>
     </Stack>
   )
 }
