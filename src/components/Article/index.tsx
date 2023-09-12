170255c2c9caf821012066732ab3728d953ea4a3
Victoria <victoria.yamashita@estudante.ufjf.br>
2023-09-12 10:04:51 -0300

    third index - shop and props

--- src/components/Article/index.tsx
+++ src/components/Article/index.tsx
@@ -1,7 +1,7 @@
 import { HStack, Stack } from "@chakra-ui/react";
 import { TextIndex } from "../Text";
 import { TitleSection } from "../Title";
-import { CardSection } from "../Card";
+import { CardSection, CardShop } from "../Card";
 import { SectionIndexTerciaryLeft, SectionIndexTerciaryRight } from "../Section";
 import { Image } from "../Image";
 
@@ -35,7 +35,7 @@ export function ArticleIndexSecundary() {
             paddingTop={32}
             spacing={32}
         >
-            <TitleSection title={"Nossos Produtos"} />
+            <TitleSection title={"Nossos Produtos"} size={"48px"} />
             <HStack
                 width={"100%"}
                 justifyContent={"space-around"}
@@ -59,7 +59,7 @@ export function ArticleIndexTerciary() {
             paddingBottom={32}
             spacing={16}
         >
-            <TitleSection title={"Crie laços, entrelaçe paixão"} />
+            <TitleSection title={"Crie laços, entrelaçe paixão"} size={"48px"} />
             <HStack
                 width={"95%"}
                 justifyContent={"space-between"}
@@ -68,6 +68,60 @@ export function ArticleIndexTerciary() {
                 <Image src={"/index_3.png"} alt={""} width={378} height={474} w="30%"/>
                 <SectionIndexTerciaryRight></SectionIndexTerciaryRight>
             </HStack>
+        </Stack>
+    )
+}
+
+interface ArticleShopProps {
+    title: string
+    color: string
+    colorCard: string
+    src1: string
+    alt1: string
+    width1: number
+    height1: number
+    titleItem1: string
+    price1: string
+    w1?: string
+    src2: string
+    alt2: string
+    width2: number
+    height2: number
+    titleItem2: string
+    price2: string
+    w2?: string
+    src3: string
+    alt3: string
+    width3: number
+    height3: number
+    titleItem3: string
+    price3: string
+    w3?: string
+}
+
+export function ArticleShop({ title, color, colorCard, src1, alt1, titleItem1, price1, width1, height1, w1, src2, alt2, titleItem2, price2, width2, height2, w2, src3, alt3, titleItem3, price3, width3, height3, w3 }:ArticleShopProps) {
+    return(
+        <Stack
+            bg={color}
+            as="article"
+            width={"100%"}
+            height={"710px"}
+            align={"center"}
+            paddingTop={32}
+            paddingBottom={32}
+            spacing={16}
+        >
+            <TitleSection title={title} size={"48px"} />
+
+            <HStack
+                width={"95%"}
+                justifyContent={"space-between"}
+            >
+                <CardShop src={src1} alt={alt1} width={width1} height={height1} title={titleItem1} text={price1} color={colorCard} w={w1}></CardShop>
+                <CardShop src={src2} alt={alt2} width={width2} height={height2} title={titleItem2} text={price2} color={colorCard} w={w2}></CardShop>
+                <CardShop src={src3} alt={alt3} width={width3} height={height3} title={titleItem3} text={price3} color={colorCard} w={w3}></CardShop>
+            </HStack>
+            
         </Stack>
     )
 }
