316dd72531475eb27b1ce2ed49b79a7ee819c191
Victoria <victoria.yamashita@estudante.ufjf.br>
2023-09-12 10:04:13 -0300

    create cardShop

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,6 +1,7 @@
 import { Box, Stack } from "@chakra-ui/react"
-import { TitleNavbar } from "../Title"
+import { TitleNavbar, TitleSection } from "../Title"
 import { ImageCard } from "../Image"
+import { TextCardShop } from "../Text"
 
 interface CardSectionProps {
     src: string
@@ -38,6 +39,38 @@ export function CardSection({ src, alt, title, width, height, w }:CardSectionPro
             >
                 Ver Produtos
             </Box>
+        </Stack>
+    )
+}
+
+interface CardShopProps {
+    src: string
+    alt: string
+    width: number
+    height: number
+    title: string
+    text: string
+    color: string
+    w?: string
+}
+
+export function CardShop({ src, alt, title, text, width, height, color, w }:CardShopProps) {
+    return (
+        <Stack
+            bg={color}
+            width={"30%"}
+            height={"395px"}
+            boxShadow={"4px 4px 10px 0px rgba(0, 0, 0, 0.25)"}
+            paddingX={12}
+            paddingY={9}
+            borderRadius={"16px"}
+            align={"center"}
+            justifyContent={"center"}
+            spacing={10}
+        >
+            <ImageCard src={src} alt={alt} width={width} height={height} w={w}/>
+            <TitleSection title={title} size={"32px"} />
+            <TextCardShop text={text}></TextCardShop>
         </Stack>
     )
 }
