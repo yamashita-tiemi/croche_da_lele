7d4c32abe531b43c4c7e371f8877c84ed92cafdc
Victoria <victoria.yamashita@estudante.ufjf.br>
2023-09-12 22:34:00 -0300

    adjusts in link/button

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,4 +1,5 @@
-import { HStack, Stack } from "@chakra-ui/react"
+import NextLink from 'next/link'
+import { HStack, Link } from "@chakra-ui/react"
 import { Image } from "../Image"
 import { TitleNavbar } from "../Title"
 
@@ -10,20 +11,28 @@ export function Navbar() {
             justifyContent={"space-between"}
             paddingX={6}
         >
-            <Image src={"/logo.png"} alt={"Crochê da Lelê"} width={491} height={125} w="20%"/>
+            <Image src={"/logo.png"} alt={"Crochê da Lelê"} width={491} height={125} w="20%" />
             <HStack
                 spacing={10}
             >
-                <TitleNavbar title={"Home"} size={"32px"} />
-                <TitleNavbar title={"Contato"} size={"32px"} />
-                <TitleNavbar title={"Membros"} size={"32px"} />
+                <Link as={NextLink} href='/' colorScheme='lilac'>
+                    <TitleNavbar title={"Home"} size={"32px"} />
+                </Link>
+                <Link as={NextLink} href='/contato'>
+                    <TitleNavbar title={"Contato"} size={"32px"} />
+                </Link>
+                <Link as={NextLink} href='/membros'>
+                    <TitleNavbar title={"Membros"} size={"32px"} />
+                </Link>
             </HStack>
             <HStack
                 spacing={10}
                 w={"20%"}
                 justifyContent={"flex-end"}
             >
-                <TitleNavbar title={"Login"} size={"32px"} />
+                <Link as={NextLink} href='/login'>
+                    <TitleNavbar title={"Login"} size={"32px"} />
+                </Link>
                 <TitleNavbar title={"Img"} size={"32px"} />
             </HStack>
         </HStack>
